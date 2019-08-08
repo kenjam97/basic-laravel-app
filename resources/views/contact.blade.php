c1ee7d2a0183f99fb56891763329506c57c3ae05
kenjam97 <jam.ken1997@gmail.com>
2019-08-09 00:18:54 +0100

    init commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title')
+    Contact
+@endsection
+
+@section('content')
+    <form>
+      <input type="text" />
+      <button>Submit</button>  
+    </form>    
+@endsection
